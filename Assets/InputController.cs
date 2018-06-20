00843a3546675e5bee296fe2bd5fba45051e79e8
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2018-06-20 10:14:27 +0530

    Animate camera rotation on user swipe

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -4,38 +4,154 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
-    private Vector3 startPosition = Vector3.zero;
-    private Vector3 endPosition = Vector3.zero;
+    public GameObject CameraRig;
+    private float cameraRigRotation;
+    private float prevCameraRigRotation;
+    private const int mMessageWidth = 200;
+    private const int mMessageHeight = 64;
 
-    private void Update()
+    private readonly Vector2 mXAxis = new Vector2(1, 0);
+    private readonly Vector2 mYAxis = new Vector2(0, 1);
+
+    private readonly string[] mMessage =
     {
-        if (Input.GetMouseButtonDown(0)) // swipe begins
+        "",
+        "Swipe Left",
+        "Swipe Right",
+        "Swipe Top",
+        "Swipe Bottom"
+    };
+
+    private int mMessageIndex = 0;
+
+    // The angle range for detecting swipe
+    private const float mAngleRange = 30;
+
+    // To recognize as swipe user should at lease swipe for this many pixels
+    private const float mMinSwipeDist = 50.0f;
+
+    // To recognize as a swipe the velocity of the swipe
+    // should be at least mMinVelocity
+    // Reduce or increase to control the swipe speed
+    private const float mMinVelocity = 2000.0f;
+
+    private Vector2 mStartPosition;
+    private float mSwipeStartTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraRigRotation = 0f;
+        prevCameraRigRotation = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mouse button down, possible chance for a swipe
+        if (Input.GetMouseButtonDown(0))
         {
-            startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Record start time and position
+            mStartPosition = new Vector2(Input.mousePosition.x,
+                Input.mousePosition.y);
+            mSwipeStartTime = Time.time;
         }
 
-        if (Input.GetMouseButtonUp(0)) // swipe ends
+        // Mouse button up, possible chance for a swipe
+        if (Input.GetMouseButtonUp(0))
         {
-            endPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
+            float deltaTime = Time.time - mSwipeStartTime;
 
-        if (startPosition != endPosition && startPosition != Vector3.zero && endPosition != Vector3.zero)
-        {
-            float deltaX = endPosition.x - startPosition.x;
-            float deltaY = endPosition.y - startPosition.y;
-            if ((deltaX > 5.0f || deltaX < -5.0f) && (deltaY >= -1.0f || deltaY <= 1.0f))
+            Vector2 endPosition = new Vector2(Input.mousePosition.x,
+                Input.mousePosition.y);
+            Vector2 swipeVector = endPosition - mStartPosition;
+
+            float velocity = swipeVector.magnitude / deltaTime;
+
+            if (velocity > mMinVelocity &&
+                swipeVector.magnitude > mMinSwipeDist)
             {
-                if (startPosition.x < endPosition.x) // swipe LTR
+                // if the swipe has enough velocity and enough distance
+
+                swipeVector.Normalize();
+
+                float angleOfSwipe = Vector2.Dot(swipeVector, mXAxis);
+                angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
+
+                // Detect left and right swipe
+                if (angleOfSwipe < mAngleRange)
                 {
-                    print("LTR");
+                    OnSwipeRight();
                 }
-                else // swipe RTL
+                else if ((180.0f - angleOfSwipe) < mAngleRange)
                 {
-                    print("RTL");
+                    OnSwipeLeft();
+                }
+                else
+                {
+                    // Detect top and bottom swipe
+                    angleOfSwipe = Vector2.Dot(swipeVector, mYAxis);
+                    angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
+                    if (angleOfSwipe < mAngleRange)
+                    {
+                        OnSwipeTop();
+                    }
+                    else if ((180.0f - angleOfSwipe) < mAngleRange)
+                    {
+                        OnSwipeBottom();
+                    }
+                    else
+                    {
+                        mMessageIndex = 0;
+                    }
                 }
             }
-
-            startPosition = endPosition = Vector3.zero;
         }
+    }
+
+    IEnumerator Rotate(Vector3 axis, float angle, float duration, GameObject objToRotate)
+    {
+        Quaternion from = objToRotate.transform.rotation;
+        Quaternion to = objToRotate.transform.rotation;
+        to *= Quaternion.Euler(axis * angle);
+
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            objToRotate.transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        objToRotate.transform.rotation = to;
+    }
+
+    void OnGUI()
+    {
+        // Display the appropriate message
+        GUI.Label(new Rect(0, 0, mMessageWidth, mMessageHeight), mMessage[mMessageIndex]);
+    }
+
+    private void OnSwipeLeft()
+    {
+        mMessageIndex = 1;
+//        CameraRig.transform.Rotate(Vector3.up, -90);
+        StartCoroutine(Rotate(Vector3.up, -90, 0.5f, CameraRig));
+    }
+
+    private void OnSwipeRight()
+    {
+        mMessageIndex = 2;
+        StartCoroutine(Rotate(Vector3.up, 90, 0.5f, CameraRig));
+    }
+
+    private void OnSwipeTop()
+    {
+        mMessageIndex = 3;
+    }
+
+    private void OnSwipeBottom()
+    {
+        mMessageIndex = 4;
     }
 }
